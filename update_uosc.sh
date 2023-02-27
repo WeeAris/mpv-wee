0d3947ba638cd38253026955cae2bd42d6feecbf
WeeAris <madoka33424@gmail.com>
2023-02-27 10:31:37 +0800

    scripts: update uosc to latest

--- update_uosc.sh
+++ update_uosc.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-mkdir -pv ./script-opts/
-rm -rf ./scripts/uosc_shared
-wget -P /tmp/ https://github.com/tomasklaen/uosc/releases/latest/download/uosc.zip
-unzip -od ./ /tmp/uosc.zip
-rm -fv /tmp/uosc.zip
-wget -NP ./scripts/ https://raw.githubusercontent.com/po5/thumbfast/master/thumbfast.lua
+#mkdir -pv ./script-opts/
+#rm -rf ./scripts/uosc_shared
+#wget -P /tmp/ https://github.com/tomasklaen/uosc/releases/latest/download/uosc.zip
+#unzip -od ./ /tmp/uosc.zip
+#rm -fv /tmp/uosc.zip
+#wget -NP ./scripts/ https://raw.githubusercontent.com/po5/thumbfast/master/thumbfast.lua
+
+git clone --depth 1 https://github.com/WeeAris/uosc-chs.git temp
+trash scripts/uosc*
+trash script-opts/
+cp -R  temp/scripts ./
+cp -R  temp/script-opts/ ./
+rm -rf temp/
